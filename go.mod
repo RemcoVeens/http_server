87d96aa89a4877ca9ab01d869029f5392eb96305
RemcoVeens <remco@track32.nl>
2025-11-11 13:39:30 +0100

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RemcoVeens/httpserver
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	servemux := http.NewServeMux()
+	servemux.Handle("/", http.FileServer(http.Dir(".")))
+	server := http.Server{
+		Handler: servemux,
+		Addr:    ":8080",
+	}
+	server.ListenAndServe()
+}
